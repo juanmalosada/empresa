b6b38891578aab85df430f7c91d36c8b38d3770d
Juan Manuel Losada <juanmalosada01@gmail.com>
2022-10-06 14:28:55 -0300

    Actualizo dependencias y entidades completas

--- src/main/java/edu/epidata/empresa/entities/Departamento.java
+++ src/main/java/edu/epidata/empresa/entities/Departamento.java
@@ -1,4 +1,94 @@
 package edu.epidata.empresa.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
 public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @OneToMany(mappedBy = "departamento")
+    private List<Empleado> empleados;
+
+    @ManyToOne
+    @JoinColumn(name = "empleado_id")
+    @JsonIgnoreProperties("departamentosACargo")
+    private Empleado jefe;
+
+    @ManyToOne
+    @JoinColumn(name = "departamento_padre_id")
+    private Departamento departamentoPadre;
+
+    @OneToMany(mappedBy = "departamentoPadre")
+    private List<Departamento> subDepartamentos;
+
+    public Departamento() {
+        super();
+    }
+    public Departamento(String nombre, List<Empleado> empleados, Empleado jefe, Departamento departamentoPadre, List<Departamento> subDepartamentos) {
+        super();
+        this.nombre = nombre;
+        this.empleados = empleados;
+        this.jefe = jefe;
+        this.departamentoPadre = departamentoPadre;
+        this.subDepartamentos = subDepartamentos;
+    }
+
+    public Departamento(String nombre, List<Empleado> empleados, Empleado jefe) {
+        super();
+        this.nombre = nombre;
+        this.empleados = empleados;
+        this.jefe = jefe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    public Empleado getJefe() {
+        return jefe;
+    }
+
+    public void setJefe(Empleado jefe) {
+        this.jefe = jefe;
+    }
+
+    public Departamento getDepartamentoPadre() {
+        return departamentoPadre;
+    }
+
+    public void setDepartamentoPadre(Departamento departamentoPadre) {
+        this.departamentoPadre = departamentoPadre;
+    }
+
+    public List<Departamento> getSubDepartamentos() {
+        return subDepartamentos;
+    }
+
+    public void setSubDepartamentos(List<Departamento> subDepartamentos) {
+        this.subDepartamentos = subDepartamentos;
+    }
+
 }
--- src/main/java/edu/epidata/empresa/entities/Empleado.java
+++ src/main/java/edu/epidata/empresa/entities/Empleado.java
@@ -1,4 +1,105 @@
 package edu.epidata.empresa.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String dni;
+
+    private String genero;
+
+    @ManyToOne
+    @JoinColumn(name = "departamento_id")
+    @JsonIgnoreProperties("empleados")
+    private Departamento departamento;
+
+    @OneToMany(mappedBy = "jefe")
+    private List<Departamento> departamentosACargo;
+
+    public Empleado() {
+        super();
+    }
+
+    public Empleado(String nombre, String apellido, String dni, String genero, Departamento departamento, List<Departamento> departamentosACargo) {
+        super();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.genero = genero;
+        this.departamento = departamento;
+        this.departamentosACargo = departamentosACargo;
+    }
+
+    public Empleado(String nombre, String apellido, String dni, String genero, Departamento departamento) {
+        super();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.genero = genero;
+        this.departamento = departamento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public List<Departamento> getDepartamentosACargo() {
+        return departamentosACargo;
+    }
+
+    public void setDepartamentosACargo(List<Departamento> departamentosACargo) {
+        this.departamentosACargo = departamentosACargo;
+    }
+
 }
